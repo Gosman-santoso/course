90f912ebcc9ced5d7b8b13a4f617873f90012643
Gosman Santoso <gosman.santoso@narate.id>
2021-06-09 21:44:28 +0700

    fix responsive

--- component/landing/about.php
+++ component/landing/about.php
@@ -4,7 +4,7 @@
       <h1 class="lengthLg1">
         Find out more about us co-learning Experiance
       </h1>
-      <p class="lengthMd1" style="margin-top: 3em; margin-bottom: 3em;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eveniet hic suscipit obcaecati praesentium natus!</p>
+      <p class="lengthMd1 text-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eveniet hic suscipit obcaecati praesentium natus!</p>
       <div class="box-image d-flex align-items-center justify-content-center">
         <img src="public/img/close-up-student-reading-book.jpg" alt="">
       </div>
@@ -12,7 +12,7 @@
     <div class="col-12 col-md-6 col-lg-6">
       <div class="row">
         <?php for ($i = 1; $i <= 4; $i++) : ?>
-          <div class="col-6 d-flex flex-column justify-content-between card">
+          <div class="col-12 col-md-6 col-lg-6 d-flex flex-column justify-content-between card">
             <i class="fas fa-book-reader card-logo align-self-start"></i>
             <p class="lengthMd1 font-bold">Learn anything</p>
             <p style="font-size: 0.8rem;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugit eveniet incidunt.</p>
--- component/navbar.php
+++ component/navbar.php
@@ -1,12 +1,12 @@
 <div class="navbar-section d-flex">
   <div class="navbar row">
-    <h1 class="col-4"><span class="color-primary">Nama</span> Course</h1>
+    <h1 class="col-6 col-md-4 col-lg-4 navbar-title d-flex align-items-center"><span class="color-primary">Nama</span> Course</h1>
     <ul class="col-4 d-flex align-items-center justify-content-between list-style-none">
       <li><a href="" class="text-decoration-none">Home</a></li>
       <li><a href="" class="text-decoration-none">About</a></li>
       <li><a href="" class="text-decoration-none">Course</a></li>
     </ul>
-    <div class="col-4 d-flex align-items-center justify-content-center login-register">
+    <div class="col-4 col-md-4 col-lg-4 d-flex align-items-center justify-content-center login-register">
       <a href="#" class="color-primary text-decoration-none">Login</a>
       <a href="#" class="color-primary text-decoration-none border-button">Register</a>
     </div>
