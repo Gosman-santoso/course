e720bc287694f6ce19a30111966df7b7a8a5dae5
Gosman Santoso <gosman.santoso@narate.id>
2021-06-16 07:18:56 +0700

    change title

--- component/navbar.php
+++ component/navbar.php
@@ -13,7 +13,7 @@ $isLogin = isset($_SESSION["login"]);
 
 <div class="navbar-section d-flex">
   <div class="navbar row">
-    <h1 class="col-6 col-md-4 col-lg-4 navbar-title d-flex align-items-center"><a href="index.php" class="text-decoration-none"><span class="color-primary">Nama</span> Course</a></h1>
+    <h1 class="col-6 col-md-4 col-lg-4 navbar-title d-flex align-items-center"><a href="index.php" class="text-decoration-none"><span class="color-primary">Our</span> Course</a></h1>
     <ul class="col-4 d-flex align-items-center justify-content-between list-style-none">
       <li><a href="index.php#home" class="text-decoration-none">Home</a></li>
       <li><a href="index.php#about" class="text-decoration-none">About</a></li>
